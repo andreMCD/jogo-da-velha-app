a7703c69268033df3bec3179dea9aa856ba48f5f
andre macedo <andredemcd@hotmail.com>
2019-07-11 16:34:29 -0300

    Criando components

---
+++ src/Components/ContentApp/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./styles.css";
+
+const Content = () => (
+  <section className="container-content">
+    <button>x</button>
+  </section>
+);
+
+export default Content;
---
+++ src/Components/HeaderApp/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./styles.css";
+
+const Container = () => (
+  <header className="container-head">
+    <h1>Jogo da Velha</h1>
+    <p>Vamos Jogar?</p>
+  </header>
+);
+
+export default Container;
